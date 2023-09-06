7cf26ba2b372877a3a7c15fa744363623d0f6ae7
Rahul Kumar <r0k067s@homeoffice.wal-mart.com>
2023-09-06 13:06:38 +0530

    update constant

---
+++ src/constant.ts
@@ -0,0 +1,28 @@
+export const content = {
+    author: 'Rahul Kumar',
+    authorTitle: 'Rahul Kumar',
+    body: 'For Windows user, Docker desktop is now available to download and install over from EDLF store (soon replaced)',
+    dateAndTime: 'Feb. 20, 2017 by',
+    hoverContent: 'Senior Software Engineer'
+  }
+  
+ export const bottomTags = [
+    {
+      color: 'red',
+      content: 'Action required by May 6th\'23',
+      id: 0,
+      variant: 'tertiary'
+    },
+    {
+      color: 'blue',
+      content: 'docker',
+      id: 1,
+      variant: 'tertiary'
+    },
+    {
+      color: 'blue',
+      content: 'docker desktop',
+      id: 2,
+      variant: 'tertiary'
+    }
+  ]
